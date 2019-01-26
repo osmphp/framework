dd4f61c10e43a557d7b28a258835537cc528023d
Vladislav Osmianskij <vo@manadev.com>
2019-01-26 07:22:24 +0200

    Type hint fix

--- src/Core/Exceptions/NotSupported.php
+++ src/Core/Exceptions/NotSupported.php
@@ -4,7 +4,7 @@ namespace Manadev\Core\Exceptions;
 
 class NotSupported extends \Exception
 {
-    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null) {
+    public function __construct($message = "", $code = 0, \Throwable $previous = null) {
         parent::__construct($message ?: 'Not supported', $code, $previous);
     }
 }
