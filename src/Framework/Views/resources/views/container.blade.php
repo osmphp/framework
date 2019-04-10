83c1f8521a167113d4cba4e01fe54958ad52df00
Vladislav Osmianskij <vo@manadev.com>
2019-04-11 00:29:05 +0300

    container with page-section modifier

--- src/Framework/Views/resources/views/container.blade.php
+++ src/Framework/Views/resources/views/container.blade.php
@@ -1,6 +1,12 @@
 <?php
 /* @var \Manadev\Framework\Views\Views\Container $view */
 ?>
+@if ($view->modifier)
+    <div class="{{ $view->modifier }}">
+@endif
 @foreach ($view->views as $child)
     @include ($child)
 @endforeach
+@if ($view->modifier)
+    </div>
+@endif
