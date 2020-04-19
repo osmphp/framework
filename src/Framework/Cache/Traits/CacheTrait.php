93cd2e5f3405371773f874b468de9aeb7c96c152
Vladislav Osmianskij <vlaosm@gmail.com>
2020-04-19 23:56:32 +0300

    minor: profiling `parent` property validation

--- src/Framework/Cache/Traits/CacheTrait.php
+++ src/Framework/Cache/Traits/CacheTrait.php
@@ -3,18 +3,26 @@
 namespace Osm\Framework\Cache\Traits;
 
 use Osm\Core\App;
+use Osm\Core\Profiler;
 use Osm\Framework\Cache\ItemValidator;
 
 trait CacheTrait
 {
     protected function around_put(callable $proceed, ...$args) {
         global $osm_app; /* @var App $osm_app */
+        global $osm_profiler; /* @var Profiler $osm_profiler */
 
-        /* @var ItemValidator $validator */
-        $validator = $osm_app[ItemValidator::class];
+        if ($osm_profiler) $osm_profiler->start('parent_validation', 'cache');
+        try {
+            /* @var ItemValidator $validator */
+            $validator = $osm_app[ItemValidator::class];
 
-        $validator->validateItem($args[1]);
+            $validator->validateItem($args[1]);
 
-        return $proceed(...$args);
+            return $proceed(...$args);
+        }
+        finally {
+            if ($osm_profiler) $osm_profiler->stop('parent_validation');
+        }
     }
 }
