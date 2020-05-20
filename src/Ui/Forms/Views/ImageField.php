10df280cdefbb7a507c9e1e537da0e56230176cd
Vladislav Osmianskij <vlaosm@gmail.com>
2020-05-20 09:35:30 +0300

    fix: image field placeholder

--- src/Ui/Forms/Views/ImageField.php
+++ src/Ui/Forms/Views/ImageField.php
@@ -4,5 +4,7 @@ namespace Osm\Ui\Forms\Views;
 
 class ImageField extends Field
 {
-
+    public $field_template = 'Osm_Ui_Forms.image-field';
+    public $view_model = 'Osm_Ui_Forms.ImageField';
+    public $type = 'image';
 }
--- src/Ui/Forms/Views/StringField.php
+++ src/Ui/Forms/Views/StringField.php
@@ -5,12 +5,5 @@ namespace Osm\Ui\Forms\Views;
 class StringField extends InputField
 {
     public $view_model = 'Osm_Ui_Forms.StringField';
-
-    protected function default($property) {
-        switch ($property) {
-            case 'type': return 'text';
-        }
-
-        return parent::default($property);
-    }
+    public $type = 'text';
 }
