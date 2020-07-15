deb1e213c3c326acb6b02664e883978e38698618
Vladislav Osmianskij <vlaosm@gmail.com>
2020-07-15 08:35:29 +0300

    fix: price field handling of empty value

--- src/Ui/Forms/resources/js/PriceField.js
+++ src/Ui/Forms/resources/js/PriceField.js
@@ -3,6 +3,10 @@ import intlParseFloat from "Osm_Framework_Js/intlParseFloat";
 
 export default class PriceField extends InputField {
     get value() {
-        return intlParseFloat(super.value);
+        let result = super.value;
+        if (!result) {
+            return result;
+        }
+        return intlParseFloat(result);
     }
 };
