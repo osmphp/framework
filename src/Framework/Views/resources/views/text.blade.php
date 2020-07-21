aca3128372a79efdbc05a37cb0eef3e14289836c
Vladislav Osmianskij <vlaosm@gmail.com>
2020-07-21 16:28:01 +0300

    fix: rendering Text::modifier property

--- src/Framework/Views/resources/views/text.blade.php
+++ src/Framework/Views/resources/views/text.blade.php
@@ -1,6 +1,6 @@
 <?php
 /* @var \Osm\Framework\Views\Views\Text $view */
 ?>
-<{{ $view->tag }} class="{{ $view->on_color_ }} {{ $view->color_ }}">
+<{{ $view->tag }} class="{{ $view->on_color_ }} {{ $view->color_ }} {{ $view->modifier }}">
     {{ $view->contents }}
 </{{ $view->tag }}>
