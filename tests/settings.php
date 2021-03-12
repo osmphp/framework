4cc623731786c5d54b91165272cccc2d32b9e2e1
Vladislav Osmianskij <vlaosm@gmail.com>
2021-03-12 13:35:39 +0200

    major: basic sheet API works

--- tests/settings.php
+++ tests/settings.php
@@ -5,31 +5,31 @@ declare(strict_types=1);
 /* @see \Osm\Framework\Settings\Hints\Settings */
 return (object)[
     'locale' => 'lt_LT',
-//    'db' => [
-//        'driver' => 'sqlite',
-//        'database' => ':memory:',
-//        'prefix' => '',
-//        'foreign_key_constraints' => true,
-//    ],
     'db' => [
-        'driver' => 'mysql',
-        'url' => $_ENV['MYSQL_DATABASE_URL'] ?? null,
-        'host' => $_ENV['MYSQL_HOST'] ?? 'localhost',
-        'port' => $_ENV['MYSQL_PORT'] ?? '3306',
-        'database' => "{$_ENV['MYSQL_DATABASE']}_test",
-        'username' => $_ENV['MYSQL_USERNAME'],
-        'password' => $_ENV['MYSQL_PASSWORD'],
-        'unix_socket' => $_ENV['MYSQL_SOCKET'] ?? '',
-        'charset' => 'utf8mb4',
-        'collation' => 'utf8mb4_unicode_ci',
+        'driver' => 'sqlite',
+        'database' => ':memory:',
         'prefix' => '',
-        'prefix_indexes' => true,
-        'strict' => true,
-        'engine' => null,
-        'options' => extension_loaded('pdo_mysql') ? array_filter([
-            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-        ]) : [],
+        'foreign_key_constraints' => true,
     ],
+//    'db' => [
+//        'driver' => 'mysql',
+//        'url' => $_ENV['MYSQL_DATABASE_URL'] ?? null,
+//        'host' => $_ENV['MYSQL_HOST'] ?? 'localhost',
+//        'port' => $_ENV['MYSQL_PORT'] ?? '3306',
+//        'database' => "{$_ENV['MYSQL_DATABASE']}_test",
+//        'username' => $_ENV['MYSQL_USERNAME'],
+//        'password' => $_ENV['MYSQL_PASSWORD'],
+//        'unix_socket' => $_ENV['MYSQL_SOCKET'] ?? '',
+//        'charset' => 'utf8mb4',
+//        'collation' => 'utf8mb4_unicode_ci',
+//        'prefix' => '',
+//        'prefix_indexes' => true,
+//        'strict' => true,
+//        'engine' => null,
+//        'options' => extension_loaded('pdo_mysql') ? array_filter([
+//            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+//        ]) : [],
+//    ],
 
     'search' => [
         'driver' => 'elastic',
