9487bef6858f93dd6f61eb40b7b9ed9fb4029e8c
Vladislav Osmianskij <vlaosm@gmail.com>
2021-03-16 06:24:23 +0200

    minor: App classes for the main app and the tools app added

---
+++ app/App.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Osm\App;
+
+use Osm\Core\App as BaseApp;
+
+class App extends BaseApp
+{
+
+}
---
+++ tools/App.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Osm\Tools;
+
+use Osm\App\App as BaseApp;
+
+class App extends BaseApp
+{
+
+}
