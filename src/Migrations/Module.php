254b459e300a223989ef9710608bd947d4d1ab11
Vladislav Osmianskij <vlaosm@gmail.com>
2021-03-17 11:27:42 +0200

    mix: missing module dependency

--- src/Migrations/Module.php
+++ src/Migrations/Module.php
@@ -9,6 +9,10 @@ use Osm\Core\BaseModule;
 
 class Module extends BaseModule
 {
+    public static array $requires = [
+        \Osm\Framework\Db\Module::class,
+    ];
+
     public static array $traits = [
         App::class => Traits\AppTrait::class,
     ];
