266d4e089c6128e67abc662303e5e1ba106674ed
Vladislav Osmianskij <vlaosm@gmail.com>
2021-08-26 23:22:43 +0300

    minor: most theme-specific Gulp scripts moved to the `base` theme

---
+++ themes/_base/gulp/css.js
@@ -0,0 +1,30 @@
+const {src, dest} = require("gulp");
+const if_ = require("gulp-if");
+const sourcemaps = require("gulp-sourcemaps");
+const csso = require("postcss-csso");
+const postcss = require("gulp-postcss");
+
+const appName = require('./appName');
+const themeName = require('./themeName');
+const config = require('./config');
+const task = require('./task');
+const bump = require("./bump");
+
+module.exports = function css() {
+    return task(`css('${appName}', '${themeName}')`, function () {
+        const plugins = [
+            require('postcss-import'),
+            require('autoprefixer')
+        ];
+        if (config.production) {
+            plugins.push(csso);
+        }
+
+        return src(`temp/${appName}/${themeName}/css/styles.css`)
+            .pipe(if_(!config.production, sourcemaps.init()))
+            .pipe(postcss(plugins))
+            .pipe(if_(!config.production, sourcemaps.write('.')))
+            .pipe(dest(`public/${appName}/${themeName}`))
+            .pipe(bump.after());
+    });
+}
