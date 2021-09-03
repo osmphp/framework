d80e61c1b05d4b1dfc4c49358e7e2ee7b6ae302a
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-03 14:42:55 +0300

    fix: handle PHP notices and warnings as exceptions

--- bin/tools.php
+++ bin/tools.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 use Osm\Runtime\Apps;
 use Osm\Tools\App;
+use function Osm\handle_errors;
 
 require 'vendor/autoload.php';
+umask(0);
+handle_errors();
 
 Apps::$project_path = getcwd();
 Apps::run(Apps::create(App::class), function (App $app) {
