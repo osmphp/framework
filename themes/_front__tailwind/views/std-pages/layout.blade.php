e1892038449071db86f517bef9cf93c5fe36082a
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-13 16:29:52 +0300

    minor: new `head` and `footer` slots in base layout template

--- themes/_front__tailwind/views/std-pages/layout.blade.php
+++ themes/_front__tailwind/views/std-pages/layout.blade.php
@@ -6,6 +6,7 @@
         <meta name="description" content="{{ $description }}">
     @endif
     <link href="{{ $asset('styles.css') }}" rel="stylesheet">
+    {{ $head }}
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
@@ -19,7 +20,7 @@
         {{ $slot }}
     </div>
     <footer class="container mx-auto">
-
+        {{ $footer }}
     </footer>
 <script src="{{ $asset('scripts.js') }}"></script>
 </body>
