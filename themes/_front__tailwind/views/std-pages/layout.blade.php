4d4bf9c3e34470734c9f899af61d3a07b09bc9f9
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-13 16:36:52 +0300

    minor: new empty `std-pages::head` Blade template for overriding in themes

--- themes/_front__tailwind/views/std-pages/layout.blade.php
+++ themes/_front__tailwind/views/std-pages/layout.blade.php
@@ -6,6 +6,7 @@
         <meta name="description" content="{{ $description }}">
     @endif
     <link href="{{ $asset('styles.css') }}" rel="stylesheet">
+    @include('std-pages::head')
     {{ $head }}
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
