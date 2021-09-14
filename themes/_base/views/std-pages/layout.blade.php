bbb42328af3d6e984c3ada3a024e01581dbdc337
Vladislav Osmianskij <vlaosm@gmail.com>
2021-09-14 12:05:39 +0300

    major: `std-pages::layout` refactored - now it's suitable for any theme

--- themes/_base/views/std-pages/layout.blade.php
+++ themes/_base/views/std-pages/layout.blade.php
@@ -5,11 +5,32 @@
     @if ($description)
         <meta name="description" content="{{ $description }}">
     @endif
+
     <link href="{{ $asset('styles.css') }}" rel="stylesheet">
+
+    @if(isset($head))
+        {{ $head }}
+    @else
+        @include('std-pages::head')
+    @endif
+
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 </head>
 <body>
+    @if(isset($header))
+        {{ $header }}
+    @else
+        @include('std-pages::header')
+    @endif
+
     {{ $slot }}
+
+    @if(isset($footer))
+        {{ $footer }}
+    @else
+        @include('std-pages::footer')
+    @endif
+
     <script src="{{ $asset('scripts.js') }}"></script>
 </body>
 </html>
