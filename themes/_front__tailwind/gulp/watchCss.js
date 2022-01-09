f86066e102f0111e7a0b6b4f9cf10b8bb70fa1b5
Vladislav Osmianskij <vlaosm@gmail.com>
2022-01-09 19:25:24 +0200

    fix: recompile TailwindCSS styles after changing Blade templates

---
+++ themes/_front__tailwind/gulp/watchCss.js
@@ -0,0 +1,25 @@
+const {watch} = require("gulp");
+
+const appName = require('./appName');
+const themeName = require('./themeName');
+const config = require('./config');
+const task = require('./task');
+const css = require('./css');
+
+module.exports = function watchCss() {
+    return task(`watchCss('${appName}', '${themeName}')`, function () {
+        let dir = `${appName}/${themeName}/css`;
+
+        let patterns = [
+            `temp/${dir}/theme/**`,
+            `temp/${dir}/styles.css`,
+            'temp/*/*/views/**/*.php',
+        ];
+        
+        config.modules.forEach(moduleName => {
+            patterns.push(`temp/${dir}/${moduleName}/**`);
+        });
+
+        return watch(patterns, css());
+    });
+}
